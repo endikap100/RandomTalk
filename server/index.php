2aa3e8dbef897745946552197b207292fb621128
JorgeNieto <jorgenietoaz@gmail.com>
2016-05-03 18:07:27 +0200

    Añadiendo mas scripts php

---
+++ server/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Jorge Nieto Azcueta - GCM server</title>
+	<!-- meta -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div>
+		<center>
+		<form action="sendGCM.php"  method="POST">
+			Title: <input type="text" name="title" size="30"><br><br>
+			Subtitle: <input type="text" name="subtitle" size="30"><br><br>
+			<textarea name="message" cols="40" rows="5" style="resize:none"></textarea>
+			<br><br>
+				Click on the button to send a message to GCM service:<br>
+			<br><br>
+			<input type="submit">
+		</form>
+		</center>
+	</div>
+</body>
+</html>
---
+++ server/sendGCM.php
@@ -0,0 +1,63 @@
+<?php
+// API access key from Google API's Console
+$registrationIds = [];
+try {
+	$mysqli = mysqli_connect("localhost","Xjnieto011","LXr19KrVEe","Xjnieto011_...");
+
+	if ($stmt = $mysqli->prepare("SELECT id FROM Dispositivos")) {
+    		// Execute the statement.
+    		$stmt->execute();
+    		// Get the variables from the query.
+            $stmt->store_result();
+            $num_of_rows = $stmt->num_rows;
+    		$stmt->bind_result($token);
+    		// Fetch the data.
+    		while($stmt->fetch()){
+                array_push($registrationIds, $token);
+            }
+
+    		$stmt->close();
+	}else{
+		printf("Error");
+	}
+	$mysqli->close();
+} catch (Exception $e) {
+	break;
+}
+define( 'API_ACCESS_KEY', 'AIzaSyDm0qcZRNdaaJoAZG1OoOHWV7oZvEWfEkI' );
+
+// prep the bundle
+$msg = array
+(
+	'message' 	=> $_POST['message'],
+	'title'		=> $_POST['title'],
+	'subtitle'	=> $_POST['subtitle'],
+	'tickerText'	=> 'Ticker text here...Ticker text here...Ticker text here',
+	'vibrate'	=> 1,
+	'sound'		=> 1,
+	'largeIcon'	=> 'large_icon',
+	'smallIcon'	=> 'small_icon'
+);
+$fields = array
+(
+	'registration_ids' 	=> $registrationIds,
+	'data'			=> $msg
+);
+ 
+$headers = array
+(
+	'Authorization: key=' . API_ACCESS_KEY,
+	'Content-Type: application/json'
+);
+ 
+$ch = curl_init();
+curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
+curl_setopt( $ch,CURLOPT_POST, true );
+curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+$result = curl_exec($ch );
+curl_close( $ch );
+echo "Message sent";
+?>
